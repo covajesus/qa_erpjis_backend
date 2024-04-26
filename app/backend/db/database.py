0f4c55c2f4bc6ef95ccae9b516f4300d35c5989e
Jesús Cova <jesuscova@jisparking.com>
2024-04-26 15:28:19 -0400

    cambios

--- app/backend/db/database.py
+++ app/backend/db/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://erpjis@erpjis:Macana11@erpjis.mysql.database.azure.com:3306/erp_jis"
-
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://erpjisv1@erpjisv1:Macana11@erpjisv1.mysql.database.azure.com:3306/erpjisv1"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_size=20, max_overflow=0)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
